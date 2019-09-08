1e051297688e4479f7e04ca8e8b3186d65ca29b5
JoseAlejandroBuitragoCardenas <54486815+JoseAlejandroBuitragoCardenas@users.noreply.github.com>
2019-09-07 21:01:55 -0500

    Initial commit

---
+++ app/src/main/java/uan/edu/co/todolist/DTO/ToDo.kt
@@ -0,0 +1,10 @@
+package uan.edu.co.todolist.DTO
+
+class ToDo {
+
+    var id: Long = -1
+    var name = ""
+    var createdAt = ""
+    var items: MutableList<ToDoItem> = ArrayList()
+
+}
---
+++ app/src/main/java/uan/edu/co/todolist/DTO/ToDoItem.kt
@@ -0,0 +1,10 @@
+package uan.edu.co.todolist.DTO
+
+class ToDoItem(){
+
+    var id : Long = -1
+    var toDoId : Long = -1
+    var itemName = ""
+    var isComleted = false
+
+}
